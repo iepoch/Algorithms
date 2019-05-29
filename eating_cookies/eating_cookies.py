924c3bb913cd176e2d7d7d3d8650f7a1c354f6b0
iepoch <steaclem@gmail.com>
2019-05-28 21:35:13 -0700

    Finished 2 solutions

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,45 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+    pass
+
+  # Fist Solution
+
+    # if n < 3:
+    #     return n
+    # elif cache and cache[n] > 0:
+    #     return cache[n]
+    # else:
+    #     if not cache:
+    #         cache = {i: 0 for i in range(n+1)}
+    #         # 3 recursive calls # n-1, n-2, n-3
+    #     cache[n] += eating_cookies(
+    #         n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    #     print(f'The num {n}')
+    #     return cache[n]
+
+
+# Second Solution
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
+
+
+eating_cookies(34)
