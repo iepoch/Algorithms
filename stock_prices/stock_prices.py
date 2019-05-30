1dde52ac7506d8b3686df1944a44679a5219bbd8
iepoch <steaclem@gmail.com>
2019-05-29 20:02:49 -0700

    Finished First Round on stocks. And on make change but make change is not working yet.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,32 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    pass
+
+    max_profits = None
+
+    for i in range(len(prices) - 1):
+        buy = prices[i]
+        sell = i + 1
+
+        for j in prices[sell:]:
+            profit = j - buy
+
+            if max_profits == None or profit > max_profits:
+                max_profits = profit
+    print(f'This is max profit {max_profits}')
+    return max_profits
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
