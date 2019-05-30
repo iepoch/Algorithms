396d82a989ae8961b2496d24b0588b8505a377ef
iepoch <steaclem@gmail.com>
2019-05-29 19:28:57 -0700

    Made new changes to eating cookies to stop the hanging issue.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,31 +8,29 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    pass
 
   # Fist Solution
-
-    # if n < 3:
-    #     return n
-    # elif cache and cache[n] > 0:
-    #     return cache[n]
-    # else:
-    #     if not cache:
-    #         cache = {i: 0 for i in range(n+1)}
-    #         # 3 recursive calls # n-1, n-2, n-3
-    #     cache[n] += eating_cookies(
-    #         n-1) + eating_cookies(n-2) + eating_cookies(n-3)
-    #     print(f'The num {n}')
-    #     return cache[n]
-
-
-# Second Solution
-    if n < 0:
-        return 0
-    elif n == 0:
+    if n == 0:
         return 1
+    elif n < 3:
+        # cache[n] = n
+        return n
+    elif cache and cache[n] > 0:
+        return cache[n]
     else:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        if not cache:
+            cache = {i: 0 for i in range(n+1)}
+            # 3 recursive calls # n-1, n-2, n-3
+        cache[n] += eating_cookies(n-1, cache) + \
+            eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        print(f'The num {n}')
+        return cache[n]
+# Second Solution
+# if n < 0:
+#     return 0
+# elif n == 0:
+# else:
+#     return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 
 if __name__ == "__main__":
