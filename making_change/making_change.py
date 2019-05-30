6f8c0d9f5c339276ebd8f40683b65ecaa3144d8a
iepoch <steaclem@gmail.com>
2019-05-29 20:52:27 -0700

    Added the making change from the Hints to the problem.

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -4,26 +4,21 @@ import sys
 
 
 def making_change(amount, denominations):
-    if amount == 0:
-        return 1
-    elif amount < 3:
-        # denominations[amount] = amount
-        return amount
-    elif denominations and denominations[amount] > 0:
-        return denominations[amount]
-    else:
-        if not denominations:
-            denominations = {i: 0 for i in range(amount+1)}
-            # 3 recursive calls # amount-1, amount-2, amount-3
-            denominations[amount] += making_change(amount-1, denominations) + making_change(
-                amount-2, denominations) + making_change(amount-3, denominations)
-    print(f'The num {amount}')
-    return denominations[amount]
+    pass
+    cache = {i: 0 for i in range(amount + 1)}
+    cache[0] = 1
+
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            pntr = higher_amount - coin
+            cache[higher_amount] += cache[pntr]
+    print(f'This is the cache amount {cache[amount]}')
+    return cache[amount]
 
 
 if __name__ == "__main__":
-        # Test our your implementation from the command line
-        # with `python making_change.py [amount]` with different amounts
+            # Test our your implementation from the command line
+            # with `python making_change.py [amount]` with different amounts
     if len(sys.argv) > 1:
         denominations = [1, 5, 10, 25, 50]
         amount = int(sys.argv[1])
