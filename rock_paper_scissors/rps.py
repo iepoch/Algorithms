01860f6abbb8fd01eb94abf0ad0780ab5ac07900
iepoch <steaclem@gmail.com>
2019-05-30 21:04:55 -0700

    Working version of rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,7 +4,22 @@ import sys
 
 
 def rock_paper_scissors(n):
-    pass
+    allPossibilities = []  # All possibilities list
+    # We will want some recurision for this
+    # Need to pass the list to each recurision call
+    # To concantate two list together
+    # Define an inner recursive helper
+    options = [['rock'], ['paper'], ['scissors']]
+    if n == 0:
+        return [[]]
+    else:
+        prevPoss = rock_paper_scissors(n-1)
+
+        for poss in prevPoss:
+            for o in options:
+                allPossibilities.append(poss + o)
+            # allPossibilities.extend(somePoss)
+    return allPossibilities
 
 
 if __name__ == "__main__":
@@ -12,4 +27,4 @@ if __name__ == "__main__":
         num_plays = int(sys.argv[1])
         print(rock_paper_scissors(num_plays))
     else:
-        print('Usage: rps.py [num_plays]')``
+        print('Usage: rps.py [num_plays]')
