746d30305674934bf66e84e2306a650c32695c0b
iepoch <steaclem@gmail.com>
2019-06-12 20:39:29 -0700

    Added eating cookies and making change

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,21 +6,38 @@ import sys
 # a solution that is more efficient than the naive
 # recursive solution
 
+# key:value
+# n : value of term
 
-def eating_cookies(n, cache=None):
-    # start with base case of 0
+
+def eating_cookies(n, cache={}):
+
+    # start with base case of
+    # Second Solution of eating cookies like the original
     if n == 0:
         return 1
-    elif n < 3:  # if n is less then 3 then return what it is
+    elif n < 3:
         return n
-    elif cache and cache[n] > 0:
+    elif n in cache:
         return cache[n]
     else:
-        if not cache:
-            cache = {i: 0 for i in range(n+1)}  # creating cache of n
-        cache[n] += eating_cookies(n-1, cache) + \
-            eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
-        return cache[n]
+        value = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        cache[n] = value
+        return value
+
+ # Working Solution
+    # if n == 0:
+    #     return 1
+    # elif n < 3:  # if n is less then 3 then return what it is
+    #     return n
+    # elif cache and cache[n] > 0:
+    #     return cache[n]
+    # else:
+    #     if not cache:
+    #         cache = {i: 0 for i in range(n+1)}  # creating cache of n
+    #     cache[n] += eating_cookies(n-1, cache) + \
+    #         eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    #     return cache[n]
 
 
 if __name__ == "__main__":
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,41 @@
 
 import sys
 
+# cache {}
+
+
 def making_change(amount, denominations):
-  pass 
+    pass
+    # base case
+    # if amount < 5 and amount >= 0:
+    #     return 1
+    # elif amount < 0:
+    #     return 0
+    # else:
+    #     # loop through the list
+    #     paths = 0
+    #     for c in denominations:
+    #         paths += making_change(amount-c, denominations)
+    #         value = paths
+    #     return value
+    cache = {i: 0 for i in range(amount + 1)}
+    cache[0] = 1
+
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            pointer = higher_amount - coin
+            cache[higher_amount] += cache[pointer]
+    print(f'There are this many ways {cache[amount]} to make {amount} cents')
+    return cache[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
