718a638fa695d56297157789b34207006613679f
iepoch <steaclem@gmail.com>
2019-06-13 18:13:42 -0700

    finished recipies

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,30 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    pass
+
+    curr_val = math.inf
+
+    for amount, value in recipe.items():
+
+        if amount not in ingredients:
+            return 0
+        value = ingredients[amount] // value
+        print(f'this is new value {amount} : {value}')
+        if value == 0:
+            return 0
+        elif value < curr_val:
+            curr_val = value
+
+    return curr_val
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
